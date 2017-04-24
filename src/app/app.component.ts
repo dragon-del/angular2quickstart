91a741da599bcaf613428b2f07c5a78d6f298dfc
erlong.duan <dragon-del@hotmail.com>
2017-04-25 07:41:36 +0900

    app module化

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,23 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  template: `
+    <app-title [subtitle]="subtitle"></app-title>
+    <nav>
+      <a routerLink="contact" routerLinkActive="active">Contact</a>
+      <a routerLink="crisis"  routerLinkActive="active">Crisis Center</a>
+      <a routerLink="heroes"  routerLinkActive="active">Heroes</a>
+    </nav>
+    <router-outlet></router-outlet>
+  `
 })
-export class AppComponent  { name = 'Angular'; }
+export class AppComponent {
+  subtitle = '(v1)';
+}
+
+
+/*
+Copyright 2017 Google Inc. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,20 @@
-import { NgModule }      from '@angular/core';
+import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppComponent }  from './app.component';
-
+/* App Root */
+import { AppComponent } from './app.component';
+/* Feature Modules */
+import { ContactModule } from './contact/contact.module';
+import { CoreModule } from './core/core.module';
+/* Routing Module */
+import { AppRoutingModule } from './app-routing.module';
 @NgModule({
-  imports:      [ BrowserModule ],
-  declarations: [ AppComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [
+    BrowserModule,
+    ContactModule,
+    CoreModule.forRoot({userName: 'Miss Marple'}),
+    AppRoutingModule
+  ],
+  declarations: [AppComponent],
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
